5171ac81203525395afe3d678e7ae6a1d5f86a12
Priyanshu Bhattacharjee <bhattacharjeepriyanshu31@gmail.com>
2026-05-15 11:52:00 +0530

    GadgetVersa project done

---
+++ javascipt/script.js
@@ -0,0 +1,88 @@
+// alert("welcome");
+let menu = document.querySelector("#menu");
+let wishList = document.querySelector("#wish-list");
+let cart = document.querySelector("#cart");
+let close = document.querySelector("#close");
+
+let navList = document.querySelector(".nav-list");
+
+menu.addEventListener("click", function(){
+   wishList.style.display = "none";
+   cart.style.display = "none";
+   menu.style.display = "none";
+   close.style.display = "block";
+    // close.style.font-size = "var(--fs)";
+    navList.classList.add("active");
+    // navList.classList.remove("nav-list");
+
+});
+
+close.addEventListener("click", function(){
+    close.style.display = "none";
+    wishList.style.display = "block";
+    cart.style.display = "block";
+    menu.style.display = "block";
+
+    // navList.classList.add("nav-list");
+    navList.classList.remove("active");
+});
+
+
+
+// Currency selector
+let currencySelector = document.querySelector("#currency-selector");
+
+// Product prices
+let prices = document.querySelectorAll(".price");
+
+// Currency conversion rates
+const rates = {
+    INR: 83,
+    USD: 1,
+    EUR: 0.92,
+    GBP: 0.78
+};
+
+// Currency symbols
+const symbols = {
+    INR: "₹",
+    USD: "$",
+    EUR: "€",
+    GBP: "£"
+};
+
+// Default currency = INR
+updatePrices("INR");
+
+// Dropdown change
+currencySelector.addEventListener("change", function () {
+    updatePrices(this.value);
+});
+
+// Function
+function updatePrices(currency) {
+
+    prices.forEach(function(price){
+
+        let basePrice = price.getAttribute("data-price");
+
+        let convertedPrice = (basePrice * rates[currency]).toFixed(0);
+
+        price.innerText = symbols[currency] + convertedPrice;
+
+    });
+
+}
+
+
+
+
+let bigImage = document.querySelector(".big-image");
+let smallImages = document.querySelectorAll(".small-image");
+
+
+smallImages.forEach(function(img){
+    img.addEventListener("click", function(){
+        bigImage.src = img.src;
+    });
+})
